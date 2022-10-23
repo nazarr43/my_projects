5508eb6cb1571581ea9d88169f1b3ca5b5f16065
nazarr43 <nazaraciv226@gmail.com>
2022-10-23 19:01:59 +0300

    commit application

---
+++ guess the number/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int numWin = random.Next(0, 1000);
+bool win = false;
+do
+{
+    Console.Write("guess a number(between 0 and 1000:");
+    string i = Console.ReadLine();
+    int numUser = int.Parse(i);
+    if(numUser < numWin)
+    {
+        Console.WriteLine("Need more, try again");
+    }
+    else if(numUser > numWin)
+    {
+            Console.WriteLine("Too much, try again");
+    }
+    else
+    {
+        Console.WriteLine("YOU ARE WIN!!!!!!!!!!!!");
+        win = true;
+    }
+}
+while (win == false);
+
